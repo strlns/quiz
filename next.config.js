d3f99a9f53888ad68f2d57e32d86400d09324229
Moritz Rehbach <dev@moritzrehbach.de>
2022-05-24 20:47:46 +0200

    Disable webpack filesystem caching on netcup shared hosting
    Filesystem caching causes problems with the paths in the "phusion passenger" jail provided by the plesk node execution environment at netcup. Absolute paths to .next/cache/webpack somehow lead to permission errors. Disable filesystem caching except for local builds.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-}
+  reactStrictMode: true,
+  webpack(config) {
+    if (process.env.IS_NETCUP_PHUSION_JAIL) {
+      config.infrastructureLogging = { debug: /PackFileCache/ };
+      config.cache = {
+        type: "memory",
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
